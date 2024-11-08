9df08194b0d883d79e9a54afa36b888627e57597
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2024-11-08 16:09:23 +0100

    refactor: convertSignleMinutesRow case 'YOOO' refactoring

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 export class Main {
   convertSimpleMinutesRow(number) {
-    if (number === 1) return "YOOO";
+    if (number % 5 === 1) return "YOOO";
     if (number === 2) return "YYOO";
     if (number === 3) return "YYYO";
     if (number === 4) return "YYYY";
-    if (number === 6) return "YOOO";
 
     return "OOOO";
   }
