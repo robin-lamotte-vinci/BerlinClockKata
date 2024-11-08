5c55cacf76a4506facf46b575f182085bc15c5f3
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2024-11-08 13:00:41 +0100

    add: convertSingleMinutesRow method context for tests

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,4 +2,8 @@ import { Main } from '../src/app.js';
 
 describe("Main class should implements a berlin clock", function() {
     const main = new Main();
+
+    describe("convertSingleMinutesRow method should return the conversion of a number into a single minutes row", function() {
+        
+    });
 });
