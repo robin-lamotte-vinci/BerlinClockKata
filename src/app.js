ba04aab5ced3f98d7e80408b7df16bc237599552
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2024-11-08 16:28:13 +0100

    refactor: convertSignleMinutesRow implementation refactoring

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 export class Main {
   convertSimpleMinutesRow(number) {
-    if (number % 5 === 1) return "YOOO";
-    if (number % 5 === 2) return "YYOO";
-    if (number % 5 === 3) return "YYYO";
-    if (number % 5 === 4) return "YYYY";
-
-    return "OOOO";
+    return "Y".repeat(number % 5) + "O".repeat(4 - number % 5);
   }
 }
