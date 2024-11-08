04fb06444ef10fc08ce91f894ed8e87662198b2f
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2024-11-08 16:30:55 +0100

    refactor: adding some semicolons

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 export class Main {
   convertSimpleMinutesRow(number) {
     return "Y".repeat(number % 5) + "O".repeat(4 - number % 5);
-  }
+  };
 
   convertFiveMinutesRow(number) {
     return "OOOOOOOOOOO";
-  }
-}
+  };
+};
