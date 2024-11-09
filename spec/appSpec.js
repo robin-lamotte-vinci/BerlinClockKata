5d8a8d1e685b7b545133d7ab16e55761b562bd4f
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2024-11-09 13:54:17 +0100

    refactor: rename const 'main' to 'berlinClockConverter'

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,71 +1,71 @@
 import { BerlinClockConverter } from "../src/app.js";
 
 describe("Main class should implements a berlin clock\n", function () {
-  const main = new BerlinClockConverter();
+  const berlinClockConverter = new BerlinClockConverter();
 
   describe("convertSingleMinutesRow method should return the conversion of a number into a single minutes row\n", function () {
     it("given 0 should return 'OOOO'", function () {
-      const result = main.convertSimpleMinutesRow(0);
+      const result = berlinClockConverter.convertSimpleMinutesRow(0);
 
       expect(result).toBe("OOOO");
     });
 
     it("given 1 should return 'YOOO'", function () {
-      const result = main.convertSimpleMinutesRow(1);
+      const result = berlinClockConverter.convertSimpleMinutesRow(1);
 
       expect(result).toBe("YOOO");
     });
 
     it("given 2 should return 'YYOO'", function () {
-      const result = main.convertSimpleMinutesRow(2);
+      const result = berlinClockConverter.convertSimpleMinutesRow(2);
 
       expect(result).toBe("YYOO");
     });
 
     it("given 3 should return 'YYYO'", function () {
-      const result = main.convertSimpleMinutesRow(3);
+      const result = berlinClockConverter.convertSimpleMinutesRow(3);
 
       expect(result).toBe("YYYO");
     });
 
     it("given 4 should return 'YYYY'", function () {
-      const result = main.convertSimpleMinutesRow(4);
+      const result = berlinClockConverter.convertSimpleMinutesRow(4);
 
       expect(result).toBe("YYYY");
     });
 
     it("given 5 should return 'OOOO'", function () {
-        const result = main.convertSimpleMinutesRow(5);
+        const result = berlinClockConverter.convertSimpleMinutesRow(5);
   
         expect(result).toBe("OOOO");
     });
 
     it("given 6 should return 'YOOO'", function () {
-        const result = main.convertSimpleMinutesRow(6);
+        const result = berlinClockConverter.convertSimpleMinutesRow(6);
   
         expect(result).toBe("YOOO");
     });
 
     it("given 7 should return 'YYOO'", function () {
-        const result = main.convertSimpleMinutesRow(7);
+        const result = berlinClockConverter.convertSimpleMinutesRow(7);
   
         expect(result).toBe("YYOO");
     });
 
     it("given 8 should return 'YYYO'", function () {
-        const result = main.convertSimpleMinutesRow(8);
+        const result = berlinClockConverter.convertSimpleMinutesRow(8);
   
         expect(result).toBe("YYYO");
     });
 
     it("given 9 should return 'YYYY'", function () {
-        const result = main.convertSimpleMinutesRow(9);
+        const result = berlinClockConverter.convertSimpleMinutesRow(9);
   
         expect(result).toBe("YYYY");
     });
 
     it("given 10 should return 'OOOO'", function () {
-        const result = main.convertSimpleMinutesRow(10);
+        const result = berlinClockConverter.convertSimpleMinutesRow(10);
   
         expect(result).toBe("OOOO");
     });
@@ -73,61 +73,61 @@ describe("Main class should implements a berlin clock\n", function () {
 
   describe("convertFiveMinutesRow method should return the conversion of a number into a five minutes row\n", function () {
     it("given 0 should return 'OOOOOOOOOOO'", function () {
-      const result = main.convertFiveMinutesRow(0);
+      const result = berlinClockConverter.convertFiveMinutesRow(0);
 
       expect(result).toBe("OOOOOOOOOOO");
     });
 
     it("given 1 should return 'OOOOOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(1);
+        const result = berlinClockConverter.convertFiveMinutesRow(1);
   
         expect(result).toBe("OOOOOOOOOOO");
     });
 
     it("given 5 should return 'YOOOOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(5);
+        const result = berlinClockConverter.convertFiveMinutesRow(5);
   
         expect(result).toBe("YOOOOOOOOOO");
     });
 
     it("given 6 should return 'YOOOOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(6);
+        const result = berlinClockConverter.convertFiveMinutesRow(6);
   
         expect(result).toBe("YOOOOOOOOOO");
     });
 
     it("given 7 should return 'YOOOOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(7);
+        const result = berlinClockConverter.convertFiveMinutesRow(7);
   
         expect(result).toBe("YOOOOOOOOOO");
     });
 
     it("given 10 should return 'YYOOOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(10);
+        const result = berlinClockConverter.convertFiveMinutesRow(10);
   
         expect(result).toBe("YYOOOOOOOOO");
     });
 
     it("given 11 should return 'YYOOOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(11);
+        const result = berlinClockConverter.convertFiveMinutesRow(11);
   
         expect(result).toBe("YYOOOOOOOOO");
     });
 
     it("given 12 should return 'YYOOOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(12);
+        const result = berlinClockConverter.convertFiveMinutesRow(12);
   
         expect(result).toBe("YYOOOOOOOOO");
     });
 
     it("given 15 should return 'YYROOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(15);
+        const result = berlinClockConverter.convertFiveMinutesRow(15);
   
         expect(result).toBe("YYROOOOOOOO");
     });
 
     it("given 16 should return 'YYROOOOOOOO'", function () {
-        const result = main.convertFiveMinutesRow(16);
+        const result = berlinClockConverter.convertFiveMinutesRow(16);
   
         expect(result).toBe("YYROOOOOOOO");
     });
