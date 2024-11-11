e1722430879af988d959e058ea45038b5a59f630
hugo dandois <hugo.dandois@student.vinci.be>
2024-11-11 18:02:13 +0100

    refactor: convertFiveMinutesRow case 'YYRYYRYYOOO'

--- src/app.js
+++ src/app.js
@@ -12,8 +12,7 @@ export class BerlinClockConverter {
   };
 
   convertFiveMinutesRow(number) {
-    if (number == 41) return "YYRYYRYYOOO"; 
-    if (number == 40) return "YYRYYRYYOOO"; 
+    if (number >= 40) return "YYRYYRYYOOO"; 
     if (number >= 35) return "YYRYYRYOOOO"; 
     if (number >= 30) return "YYRYYROOOOO"; 
     if (number >= 25) return "YYRYYOOOOOO";
