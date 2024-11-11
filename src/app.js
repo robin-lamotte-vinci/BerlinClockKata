1999f2dde5754eab38a1ee386c23ee2258f0360f
Robin Lamotte <163841689+robin-lamotte-vinci@users.noreply.github.com>
2024-11-11 19:34:05 +0100

    refactor: convertFiveMinutesRow() (+ extracted methods) renaming the argument's name from 'number' to 'minutes' for more lisibility

--- src/app.js
+++ src/app.js
@@ -11,12 +11,12 @@ export class BerlinClockConverter {
     return "O".repeat(4 - (minutes % 5));
   };
 
-  convertFiveMinutesRow(number) {
-    return this.#getTurnedOnLightsForFiveMinutes(number) + this.#getTurnedOffLightsForFiveMinutes(number);
+  convertFiveMinutesRow(minutes) {
+    return this.#getTurnedOnLightsForFiveMinutes(minutes) + this.#getTurnedOffLightsForFiveMinutes(minutes);
   };
 
-  #getTurnedOnLightsForFiveMinutes(number) {
-    const numberOfTurnedOnLights = Math.floor(number / 5);
+  #getTurnedOnLightsForFiveMinutes(minutes) {
+    const numberOfTurnedOnLights = Math.floor(minutes / 5);
 
     let turnedOnLights = "";
     for (let i = 1; i <= numberOfTurnedOnLights; i++) {
@@ -25,8 +25,8 @@ export class BerlinClockConverter {
     return turnedOnLights;
   };  
   
-  #getTurnedOffLightsForFiveMinutes(number) {
-    const numberOfTurnedOffLights = 11 - Math.floor(number / 5);
+  #getTurnedOffLightsForFiveMinutes(minutes) {
+    const numberOfTurnedOffLights = 11 - Math.floor(minutes / 5);
 
     return "O".repeat(numberOfTurnedOffLights);
   };
