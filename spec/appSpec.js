c596540709f13b3fbd0d7272f6ff5bc2377486ea
Popgeorgiev <georgi.popgeorgiev@student.vinci.be>
2024-11-12 19:35:36 +0100

    add : convertFiveHoursRow case 20

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -332,5 +332,11 @@ describe("BerlinClockConverter class should implements the conversion time in a 
       expect(result).toBe("RRRO");
     });
 
+    it("given 20 should return 'RRRR'", function () {
+      const result = berlinClockConverter.convertFiveHoursRow(20);
+
+      expect(result).toBe("RRRR");
+    });
+
   });
 });
